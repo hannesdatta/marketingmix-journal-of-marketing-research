cb3efbee7ecc01cee4ace2d76cb05d26946414b9
Hannes Datta <h.datta@tilburguniversity.edu>
2017-01-31 15:44:18 +0100

    pull in data from derived, update makefile

--- analysis/code/get_externals.bat
+++ analysis/code/get_externals.bat
@@ -1,10 +1,38 @@
-set svnpath=https://svn.uvt.nl/tisem-datta/DATTA_GfkSingapore
+@echo off
+
+ECHO.
+ECHO ...............................................
+ECHO PRESS 1 or 2 to select your task, or 3 to EXIT.
+ECHO ...............................................
+ECHO.
+ECHO 1 - Retrieve externals from S3
+ECHO 2 - Retrieve externals from \derived (if you have built the project completely on your system)
+ECHO 3 - EXIT
+ECHO.
+
+SET /P M=Type 1, 2, or 3 then press ENTER:
+IF %M%==1 GOTO AWS
+IF %M%==2 GOTO RAW
+IF %M%==3 GOTO EOF
+
+
+:AWS
 RMDIR ..\external /S /Q
 mkdir ..\external
-mkdir ..\external\datasets
+echo Download externals from AWS S3
 
-REM DATA FROM REPOSITORY 
-set rev=HEAD
-REM alternatively, rev=1 (e.g., revision 1)
-REM svn export "%svnpath%/derived/out/@%rev%" "../external/datasets/" --force -r %rev%
+REM curl -o ../external/matching_stats.txt https://s3.eu-central-1.amazonaws.com/uvt-spotify/builds/pre-analysis/2016_09_18/matching_stats.txt -k
+
+GOTO EXIT_PART
+
+:RAW
+RMDIR ..\external /S /Q
+mkdir ..\external
+ 
+echo Copy externals from local repository
+copy ..\..\derived\output\*.csv ..\external\
+
+GOTO EXIT_PART
+
+:EXIT_PART
 pause
