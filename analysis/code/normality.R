3a2b504ebbdd4bcabae6db5af58862769924112c
Hannes Datta <h.datta@tilburguniversity.edu>
2017-03-27 16:53:20 +0200

    make first changes to normality assessment (log and non-log)

--- analysis/code/normality.R
+++ analysis/code/normality.R
@@ -1,3 +1,24 @@
+#     _____    __   _  __                               _                 _       
+#    / ____|  / _| | |/ /       /\                     | |               (_)      
+#   | |  __  | |_  | ' /       /  \     _ __     __ _  | |  _   _   ___   _   ___ 
+#   | | |_ | |  _| |  <       / /\ \   | '_ \   / _` | | | | | | | / __| | | / __|
+#   | |__| | | |   | . \     / ____ \  | | | | | (_| | | | | |_| | \__ \ | | \__ \
+#    \_____| |_|   |_|\_\   /_/    \_\ |_| |_|  \__,_| |_|  \__, | |___/ |_| |___/
+#                                                            __/ |                
+#                                                           |___/                 
+#
+#  _________________________
+# |  _______________  |     |
+# | |               | |1 2 3|
+# | |               | |4 5 6|
+# | |               | |7 8 9|
+# | |               | |# # #|
+# | |               | | ___ |
+# | |_______________| ||___|| 
+# |___________________|_____|
+
+# Purpose: Assess required non-normality of marketing mix instruments for the application of Gaussian Copula correction terms
+
 
 ### LOAD DATA SETS
 require(data.table)
@@ -21,6 +42,8 @@ require(data.table)
 
 	}
 
+	init()
+	
 ### Import data
 	brand_panel=fread('..\\..\\derived\\output\\datasets.csv')
 	brand_panel[, ':=' (date = as.Date(date))]
@@ -31,29 +54,36 @@ require(data.table)
 	void<-clusterEvalQ(cl, init())
 	void<-clusterEvalQ(cl, require(data.table))
 		
-
 # Retrieve variables for testing
 	dt=melt(brand_panel[selected_t_brand==T, !colnames(brand_panel)%in%c('selected_t_brand', 'selected_t_cat', 'selected'),with=F], id.vars=c('country', 'brand', 'date', 'category', 'market_id'))
-	dt=dt[market_id%in%1:10000 & variable%in%c('rwpspr', 'wpswdst', 'llen', 'nov3')]
-	dt[variable=='rwpspr', value:=(value)]
-	dt[variable=='llen', value:=(value)]
-	dt[variable=='wpswdst', value:=(value)]
-	dt[variable=='nov3', value:=(value)]
-	#dt[variable=='nov3', value:=log(value+1)]
 	
+	# retain only necessary variables
+	dt=dt[variable%in%c('rwpspr', 'wpswdst', 'llen', 'nov3', 'wpsun')]
+	
+	# apply (log) transformations
+	dt[variable=='rwpspr', lvalue:=log(value)]
+	dt[variable=='wpswdst', lvalue:=log(value+1)]
+	dt[variable=='llen', lvalue:=log(value)]
+	dt[variable=='nov3', lvalue:=log(value+1)]
+	dt[variable=='wpsun', lvalue:=log(value+1)]
+	
+# Verify whether each series is to be used; if not, kick out
+	dt[, ts_selected := use_ts(value), by=c('country','category', 'brand', 'variable')]
+	
+	dt <- dt[ts_selected==T]
+	dt[,ts_selected:=NULL]
 
 # Check for UR using the cluster
 	split_dt = split(dt, dt$market_id)
 
-
-	try_adf <- function(x) {
+	cluster_adf <- function(x) {
 		out = try({adf_enders(x, maxlag=12, pval=.05,season=NULL)["ur"]}, silent=T)
 		if (class(out)=='try-error') return(as.numeric(NA)) else return(out)
 		}
-	clusterExport(cl, 'try_adf')
+	clusterExport(cl, 'cluster_adf')
 			
 	out = parLapplyLB(cl, split_dt, function(i) {
-					i[, list(ur = try_adf(value)), by=c('market_id', 'category', 'country', 'brand', 'variable')]
+					i[, list(ur_nonlog = cluster_adf(value), ur_log = cluster_adf(lvalue)), by=c('market_id', 'category', 'country', 'brand', 'variable')]
 					})
 
 	urs = rbindlist(out)
@@ -61,24 +91,37 @@ require(data.table)
 	setkeyv(dt, c('market_id', 'category', 'country', 'brand', 'variable'))
 	setkeyv(urs, c('market_id', 'category', 'country', 'brand', 'variable'))
 
-	dt[, ur:=NULL]
-	dt[urs, ur:=i.ur]
+	dt[, ':=' (ur_nonlog=NULL, ur_log=NULL)]
+	dt[urs, ':=' (ur_nonlog=i.ur_nonlog, ur_log = i.ur_log)]
 
 # Transform to first-differences if UR is found	
-	dt[ur==1, transfvalue := makediff(value), by=c('market_id', 'category', 'country', 'brand', 'variable')]
-	dt[ur==0, transfvalue := value, by=c('market_id', 'category', 'country', 'brand', 'variable')]
+	dt[ur_nonlog==1, transfvalue_nonlog := makediff(value), by=c('market_id', 'category', 'country', 'brand', 'variable')]
+	dt[ur_nonlog==0, transfvalue_nonlog := value, by=c('market_id', 'category', 'country', 'brand', 'variable')]
+	
+	dt[ur_log==1, transfvalue_log := makediff(lvalue), by=c('market_id', 'category', 'country', 'brand', 'variable')]
+	dt[ur_log==0, transfvalue_log := lvalue, by=c('market_id', 'category', 'country', 'brand', 'variable')]
+	
 	dt <- dt[!is.na(transfvalue)]
 	
 # Conduct Shapiro-Wilk tests on levels and differenced series
-	dt[, shapiro_p_diff := shapiro.test(transfvalue)$p, by=c('market_id', 'category', 'country', 'brand', 'variable')]
-	dt[, shapiro_p_lev := shapiro.test(value)$p, by=c('market_id', 'category', 'country', 'brand', 'variable')]
+	# non-logs
+	dt[!is.na(transfvalue_nonlog), shapiro_p_diff_nonlog := shapiro.test(transfvalue_nonlog)$p, by=c('market_id', 'category', 'country', 'brand', 'variable')]
+	dt[!is.na(value), shapiro_p_lev_nonlog := shapiro.test(value)$p, by=c('market_id', 'category', 'country', 'brand', 'variable')]
 
-	dt[, shapiro_p_lev_pooled := shapiro.test(value)$p, by=c('market_id', 'category', 'country', 'variable')]
+	# logs
+	dt[!is.na(transfvalue_log), shapiro_p_diff_log := shapiro.test(transfvalue_log)$p, by=c('market_id', 'category', 'country', 'brand', 'variable')]
+	dt[!is.na(value), shapiro_p_lev_log := shapiro.test(lvalue)$p, by=c('market_id', 'category', 'country', 'brand', 'variable')]
 
 # Summarize results
-	summ <- dt[, list(shapiro_p_diff = unique(shapiro_p_diff), shapiro_p_lev = unique(shapiro_p_lev), ur=unique(ur)), by=c('market_id', 'category', 'country', 'brand', 'variable')]
-	summ2 <- dt[, list(shapiro_p = unique(shapiro_p_lev_pooled)), by=c('market_id', 'category', 'country', 'variable')]
-
+	summ <- dt[, lapply(.SD, function(x) unique(x[!is.na(x)])), by = c('market_id', 'category', 'country', 'brand', 'variable'), .SDcols=c('ur_nonlog', 'ur_log', grep('shapiro_p', colnames(dt), value=T))]
+	setnames(summ, 'variable', 'regressor')
+	summ <- melt(summ, id.vars=c('market_id','category','country','brand','regressor','ur_nonlog', 'ur_log'))
+	
+# Summarize test outcomes
+	# non-logs
+	summ[grepl('_nonlog', variable), list(length(which(value<.1))/.N), by = c('ur_nonlog', 'variable')]
+	
+	
 # Number of true (= non-normal) test outcomes
 	table(summ$shapiro_p_lev<.10)
 	table(summ$shapiro_p_diff<.10)
