ed5237b46e4d4e2cd7bc972f838c93cd8a439c43
Hannes Datta <h.datta@tilburguniversity.edu>
2017-03-27 14:33:02 +0200

    changes to normality evaluation w Harald and Marnik

--- analysis/code/normality.R
+++ analysis/code/normality.R
@@ -21,24 +21,26 @@ require(data.table)
 
 	}
 
-# Start cluster
-	cl<-makePSOCKcluster(ncpu)
-	void<-clusterEvalQ(cl, init())
-	void<-clusterEvalQ(cl, require(data.table))
-		
 ### Import data
 	brand_panel=fread('..\\..\\derived\\output\\datasets.csv')
 	brand_panel[, ':=' (date = as.Date(date))]
 
+# Start cluster
+	cl<-makePSOCKcluster(ncpu)
 	clusterExport(cl,c('brand_panel', 'init'))
+	void<-clusterEvalQ(cl, init())
+	void<-clusterEvalQ(cl, require(data.table))
+		
 
 # Retrieve variables for testing
 	dt=melt(brand_panel[selected_t_brand==T, !colnames(brand_panel)%in%c('selected_t_brand', 'selected_t_cat', 'selected'),with=F], id.vars=c('country', 'brand', 'date', 'category', 'market_id'))
-	dt=dt[market_id%in%1:1000 & variable%in%c('rwpsprice', 'wpswdist', 'llength', 'novel')]
-	dt[variable=='rwpsprice', value:=log(value)]
-	dt[variable=='llength', value:=log(value)]
-	dt[variable=='wpswdist', value:=log(value+1)]
-	dt[variable=='novel', value:=log(value+1)]
+	dt=dt[market_id%in%1:10000 & variable%in%c('rwpspr', 'wpswdst', 'llen', 'nov3')]
+	dt[variable=='rwpspr', value:=(value)]
+	dt[variable=='llen', value:=(value)]
+	dt[variable=='wpswdst', value:=(value)]
+	dt[variable=='nov3', value:=(value)]
+	#dt[variable=='nov3', value:=log(value+1)]
+	
 
 # Check for UR using the cluster
 	split_dt = split(dt, dt$market_id)
@@ -66,23 +68,33 @@ require(data.table)
 	dt[ur==1, transfvalue := makediff(value), by=c('market_id', 'category', 'country', 'brand', 'variable')]
 	dt[ur==0, transfvalue := value, by=c('market_id', 'category', 'country', 'brand', 'variable')]
 	dt <- dt[!is.na(transfvalue)]
+	
+# Conduct Shapiro-Wilk tests on levels and differenced series
+	dt[, shapiro_p_diff := shapiro.test(transfvalue)$p, by=c('market_id', 'category', 'country', 'brand', 'variable')]
+	dt[, shapiro_p_lev := shapiro.test(value)$p, by=c('market_id', 'category', 'country', 'brand', 'variable')]
 
-# Conduct Shapiro-Wilk tests
-	dt[, shapiro_p := shapiro.test(transfvalue)$p, by=c('market_id', 'category', 'country', 'brand', 'variable')]
+	dt[, shapiro_p_lev_pooled := shapiro.test(value)$p, by=c('market_id', 'category', 'country', 'variable')]
 
 # Summarize results
-	summ <- dt[, list(shapiro_p = unique(shapiro_p), ur=unique(ur)), by=c('market_id', 'category', 'country', 'brand', 'variable')]
+	summ <- dt[, list(shapiro_p_diff = unique(shapiro_p_diff), shapiro_p_lev = unique(shapiro_p_lev), ur=unique(ur)), by=c('market_id', 'category', 'country', 'brand', 'variable')]
+	summ2 <- dt[, list(shapiro_p = unique(shapiro_p_lev_pooled)), by=c('market_id', 'category', 'country', 'variable')]
 
 # Number of true (= non-normal) test outcomes
-	table(summ$shapiro_p<.10)
+	table(summ$shapiro_p_lev<.10)
+	table(summ$shapiro_p_diff<.10)
+	table(summ2$shapiro_p<.10)
 
 # Share of non-normally distributed variables
-	length(which(summ$shapiro_p<.10))/length(summ$shapiro_p)
+	length(which(summ$shapiro_p_lev<.10))/length(summ$shapiro_p_lev)
+	length(which(summ$shapiro_p_diff<.10))/length(summ$shapiro_p_diff)
 
 # Test outcomes by variable and unit root outcome
-	resdt = summ[, list(length(which(shapiro_p<.10))/.N),  by = c('ur', 'variable')]
+	resdt = summ[, list(length(which(shapiro_p_diff<.10))/.N),  by = c('ur', 'variable')]
 	dcast(resdt, variable ~  ur)
 
+	resdt = summ[, list(length(which(shapiro_p_lev<.10))/.N),  by = c('ur', 'variable')]
+	dcast(resdt, variable ~  ur)
+	
 # How many brands
 	nrow(dt[, list(.N), by = c('brand', 'market_id' )])
 
