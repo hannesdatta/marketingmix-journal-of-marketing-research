3487e275d8f0e45aeb0d844dff110aac20de9b81
Hannes Datta <h.datta@tilburguniversity.edu>
2018-02-28 17:26:45 +0100

    remove legacy models; streamline everything to get to new model estimates as per discussion w/ harald&marnik

--- analysis/code/analysis.R
+++ analysis/code/analysis.R
@@ -32,9 +32,9 @@ require(bit64)
 	tmp[, Ncountries := .N, by = c('brand')]
 	tmp[, N:=NULL]
 	tmp[, present := T]
-	tmp = dcast(tmp, brand + Ncountries ~ country, value.var='present')
+	tmp = dcast.data.table(tmp, brand + Ncountries ~ country, value.var='present')
 	
-	tmp[, Ncountries:=-Ncountries]
+	tmp[, Ncountries:= -Ncountries]
 	setorder(tmp, Ncountries, brand)
 	tmp[, Ncountries:=-Ncountries]
 	write.table(tmp, 'clipboard', row.names=F)
@@ -44,7 +44,7 @@ require(bit64)
 	require(parallel)
 	
 ### Setup cluster environment
-	ncpu = 4#10
+	ncpu = 10 #4
 	
 ### Initialize all required functions (possibly on the cluster, too)
 
@@ -81,7 +81,7 @@ require(bit64)
 	m1 <- list(setup_y=c(usalessh = 'usalessh'),
 		   setup_x=c(price = 'rwpspr', dist = 'wpswdst', llen = 'llen', nov = 'nov6sh'),
 		   setup_endogenous = c('rwpspr', 'wpswdst','llen','nov6sh'),
-		   trend='none', # choose from: all, ur, none.
+		   trend='none', # choose from: always, ur, none.
 		   pval = .1,
 		   max.lag = 12, 
 		   min.t = 36,
@@ -90,7 +90,7 @@ require(bit64)
 		   benchmarkb = NULL,
 		   estmethod = "FGLS-Praise-Winsten",
 		   attraction_model = "MNL",
-		   takediff = 'alwaysdiff',
+		   takediff = 'none', # choose from flexible (UR approach), none, alwaysdiff
 		   use_quarters = F,
 		   plusx = NULL, #c('nov3sh', 'wpswdst'),
 		   squared=F,
@@ -126,16 +126,16 @@ assign_model(m1)
 		                                             setup_endogenous = setup_endogenous, 
 		                                             trend = trend, pval = pval, max.lag = max.lag, 
 		                                             min.t = min.t, maxiter=maxiter, attraction_model = attraction_model, 
-		                                             plusx=plusx, use_quarters=use_quarters, squared=squared), silent=T)
+		                                             plusx=plusx, use_quarters=use_quarters, squared=squared, takediff=takediff), silent=T)
 		
 		assign_model(m1, del = TRUE)
 		
 	}
 	assign_model(m1, del = TRUE)
 	
-	######################
+	################
 	# Cluster			 #
-	######################
+	################
 	
 # set up cluster
 
@@ -156,31 +156,27 @@ assign_model(m1)
 	Sys.time()
 	
 	results_MNL <- parLapplyLB(cl, analysis_markets[1:last.item], function(i) {
-	    if(i==27) {maxit=30000} else {maxit=400}
-			try(analyze_by_market(i, setup_y = setup_y, setup_x = setup_x, setup_endogenous = setup_endogenous, trend = trend, pval = pval, max.lag = max.lag, min.t = min.t, maxiter = maxit, use_quarters=F, plusx=plusx, attraction_model=attraction_model, squared=squared),silent=T)
+	    #if(i==27) {maxit=30000} else {maxit=400}
+	    maxit=400
+			try(analyze_by_market(i, setup_y = setup_y, setup_x = setup_x, setup_endogenous = setup_endogenous, trend = trend, pval = pval, max.lag = max.lag, min.t = min.t, maxiter = maxit, use_quarters=F, plusx=plusx, attraction_model=attraction_model, squared=squared, takediff=takediff),silent=T)
 			})
-	results_MNL[[124]]=analyze_by_market(138, setup_y = setup_y, setup_x = c(price = 'rwpspr', dist = 'wpswdst', nov = 'nov6sh'), setup_endogenous = c(price = 'rwpspr', dist = 'wpswdst', nov = 'nov6sh'), trend = 'none', maxiter=300, use_quarters=F, estmethod='FGLS-Praise-Winsten',squared=squared)
 	
 	Sys.time()
 	
-	save(results_MNL, analysis_markets, m1, file = c('../temp/results_20180205.RData'))
+	save(results_MNL, analysis_markets, m1, file = c('../temp/results_20180228.RData'))
 	
-	#	What if ??_b (or ??_B) are negative, or not bounded between zero and one?
-	#-	Note: Els/Bernadette compute elasticities only in cases where both ms and X have UR, or not. NOT for mixed cases, but exclude it from paper.
 	
-	# MNL with quarter	
-	assign_model(m1, del = TRUE)
-	assign_model(m1)
-	clusterExport(cl,names(m1))
-	
-	Sys.time()
-	results_MNL_wquarter <- parLapplyLB(cl, analysis_markets[1:last.item], function(i) {
-	  if(i==27) {maxit=30000} else {maxit=400}
-	  try(analyze_by_market(i, setup_y = setup_y, setup_x = setup_x, setup_endogenous = setup_endogenous, trend = trend, pval = pval, max.lag = max.lag, min.t = min.t, maxiter = maxit, use_quarters=T, plusx=plusx, attraction_model=attraction_model),silent=T)
+	results_MNLw_quarter <- parLapplyLB(cl, analysis_markets[1:last.item], function(i) {
+	  #if(i==27) {maxit=30000} else {maxit=400}
+	  maxit=400
+	  try(analyze_by_market(i, setup_y = setup_y, setup_x = setup_x, setup_endogenous = setup_endogenous, trend = 'always', pval = pval, max.lag = max.lag, min.t = min.t, maxiter = maxit, use_quarters=T, plusx=plusx, attraction_model=attraction_model, squared=squared, takediff=takediff),silent=T)
 	})
+	
 	Sys.time()
 	
-	# MCI without quarter
+	save(results_MNL,results_MNLw_quarter, analysis_markets, m1, file = c('../temp/results_20180228.RData'))
+	
+	# MCI 
 	m1$plusx=c('nov6sh', 'wpswdst')
 	m1$attraction_model='MCI'
 	assign_model(m1, del = TRUE)
@@ -188,125 +184,17 @@ assign_model(m1)
 	clusterExport(cl,names(m1))
 	
 	results_MCI <- parLapplyLB(cl, analysis_markets[1:last.item], function(i) {
-	  if(i==27) {maxit=30000} else {maxit=400}
-	  try(analyze_by_market(i, setup_y = setup_y, setup_x = setup_x, setup_endogenous = setup_endogenous, trend = trend, pval = pval, max.lag = max.lag, min.t = min.t, maxiter = maxit, use_quarters=F, plusx=plusx, attraction_model=attraction_model),silent=T)
+	   	    maxit = 400
+	    	  try(analyze_by_market(i, setup_y = setup_y, setup_x = setup_x, setup_endogenous = setup_endogenous, trend = 'always', pval = pval, max.lag = max.lag, min.t = min.t, maxiter = maxit, use_quarters=T, plusx=plusx, attraction_model=attraction_model,squared=squared,takediff=takediff),silent=T)
 	})
-	results_MCI[[124]]=analyze_by_market(138, setup_y = setup_y, setup_x = c(price = 'rwpspr', dist = 'wpswdst', nov = 'nov3sh'), setup_endogenous = c(price = 'rwpspr', dist = 'wpswdst', nov = 'nov3sh'), trend = trend, maxiter=300, use_quarters=F, estmethod='FGLS-Praise-Winsten')
 	
-  save(results_MNL, results_MCI, results_MNL_wquarter, analysis_markets, m1, file = c('../temp/results_20171113.RData'))
+	save(results_MNL,results_MNLw_quarter,results_MCI, analysis_markets, m1, file = c('../temp/results_20180228.RData'))
 
-    Sys.time()
-
- # run up to this point. 
-
-  ################
-  # DESCRIPTIVES #
-  ################
-
-    load(file = c('../temp/results_20171113.RData'))
-    
-  get_elast <- function(results_brands) {
-    checks <- unlist(lapply(results_brands, class))
-    
-    cat('error report:\n\n')
-    print(table(checks))
-  
-    # elasticities
-    elast <- rbindlist(lapply(results_brands[!checks=='try-error'], function(x) x$elast))
-  
-    zval=1.64
-    out=elast[!is.na(coef), list(median_elast = median(elast), 
-                           w_elast = sum(elast/elast_se)/sum(1/elast_se), 
-                           N_brands= .N, 
-                           perc_positive = length(which(z>=(zval)))/.N, 
-                           perc_null = length(which(abs(z)<zval))/.N, 
-                           perc_negative = length(which(z<=(-zval)))/.N), by=c('variable')]
-    res=list(checks=checks, elast=out)
-    return(res)
-  }
-  
-  m = get_elast(results_MNL)
-  
-  
-  
-  m3 = get_elast(results_MNL)
-  m6 = get_elast(results_MNL_6sh)
-  
-  {
-  cat('Summary of elasticities with novelty share 3-months\n')
-  print(m3$elast)
-  cat('\n\nSummary of elasticities with novelty share 6-months\n')
-  print(m6$elast)
-  }
-
-  
-  
-  
-#####################
-# summarize results #
-#####################
-
-		load(file = c('../temp/results_noquarter.RData'))
+	# MCI  without trend
+	results_MCI_notrend <- parLapplyLB(cl, analysis_markets[1:last.item], function(i) {
+	  maxit = 400
+	  try(analyze_by_market(i, setup_y = setup_y, setup_x = setup_x, setup_endogenous = setup_endogenous, trend = 'none', pval = pval, max.lag = max.lag, min.t = min.t, maxiter = maxit, use_quarters=T, plusx=plusx, attraction_model=attraction_model,squared=squared,takediff=takediff),silent=T)
+	})
 	
-	checks <- unlist(lapply(results_brands, class))
-	table(checks)
+	save(results_MNL,results_MNLw_quarter,results_MCI,results_MCI_notrend, analysis_markets, m1, file = c('../temp/results_20180228.RData'))
 	
-	
-	# elasticities
-	elast <- rbindlist(lapply(results_brands[!checks=='try-error'], function(x) x$elast))
-
-	zval=1.69
-	elast[!is.na(coef), list(median_elast = median(elast), 
-												  w_elast = sum(elast/elast_se)/sum(1/elast_se), 
-												  N_brands= .N, 
-												  perc_positive = length(which(z>=(zval)))/.N, 
-												  perc_null = length(which(abs(z)<zval))/.N, 
-												  perc_negative = length(which(z<=(-zval)))/.N), by=c('variable')]
-		
-	# copulas
-	copulas <- rbindlist(lapply(results_brands[!checks=='try-error'], function(x) x$model@coefficients))
-	copulas <- copulas[grepl('cop_', varname)]
-	
-	copulas[!is.na(coef), list(median = median(coef), 
-											  weighted = sum(coef/se)/sum(1/se), 
-											  N_brands= .N, 
-											  perc_positive = length(which(z>=(zval)))/.N, 
-											  perc_null = length(which(abs(z)<zval))/.N, 
-											  perc_negative = length(which(z<=(-zval)))/.N), by=c('varname')]
-	# seasonality
-	season <- rbindlist(lapply(results_brands[!checks=='try-error'], function(x) x$model@coefficients))
-	season <- season[grepl('quarter[0-9]_', variable)]
-	
-	season[!is.na(coef), list(median = median(coef), 
-											  weighted = sum(coef/se)/sum(1/se), 
-											  N_brands= .N, 
-											  perc_positive = length(which(z>=(zval)))/.N, 
-											  perc_null = length(which(abs(z)<zval))/.N, 
-											  perc_negative = length(which(z<=(-zval)))/.N), by=c('brand')]
-												  
-	
-	# plot histograms of elasticities
-	for (vars in c('llen', 'rwpspr', 'wpsun'))
-	
-	fn = '../output/elast2.png'
-	
-	png(fn, res=400, units='in', height=8, width=8)
-	
-	par(mfrow=c(3,2))
-	hist(elast[variable=='llen']$elast, breaks=100, col='grey', main = 'llen', xlab = 'elasticity')
-	hist(elast[variable=='rwpspr']$elast, breaks=100, col='grey', main = 'price', xlab = 'elasticity') # -15
-	hist(elast[variable=='wpsun']$elast, breaks=100, col='grey', main = 'unique', xlab = 'elasticity')
-	hist(elast[variable=='wpswdst']$elast, breaks=100, col='grey', main = 'dist', xlab = 'elasticity')
-	hist(elast[variable=='nov3sh']$elast, breaks=100, col='grey', main = 'noveltyshare', xlab = 'elasticity')
-	
-	dev.off()
-	
-	elast[variable=='llen' & abs(elast) > 6]
-	
-	elast[variable=='rwpspr' & abs(elast) > 6]
-	
-
-	elast[variable=='llen' & abs(elast) > 6]
-	# check data / model outputs for top and bottom 10; plus 5+
-	elast[variable=='rwpspr' & elast > 6]
-
