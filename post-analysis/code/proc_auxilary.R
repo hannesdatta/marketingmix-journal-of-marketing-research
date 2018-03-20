131b1dcb964a006e9c6997af8e6ef613a345e9f7
Hannes Datta <hannes@datta-online.de>
2018-03-20 23:51:52 +0100

    implement mean centering; reproduce today"s meeting results

--- post-analysis/code/proc_auxilary.R
+++ post-analysis/code/proc_auxilary.R
@@ -2,18 +2,8 @@
 regmodel <- function(formula=~1+I(country_class=='linc') + as.factor(category) + as.factor(brand), 
                      dat) {
 
-  # extract variable names from formula / model frame
-  
-  mc = ~ln_gdppercap
-  library(rockchalk)
-  
-  
-  source('meancenter2.R')
-  
   regs <- lapply(unique(dat$variable), function(varname) {
       st<-lm(update(formula, elast ~ .), data = data.table(dat[variable==varname&!is.na(elast)]), weights=1/elast_se)
-      st2=meancenter2(st)
-      
       lt<-lm(update(formula, elastlt ~ .), data = data.table(dat[variable==varname&!is.na(elastlt)]), weights=1/elastlt_se)
       
       return(list(variable=varname, st=st, lt=lt))
