18cc69939d85da4f835b3e1276e33fa9d13efc4e
Hannes Datta <h.datta@tilburguniversity.edu>
2018-03-21 10:43:10 +0100

    implement adding different models side by side (without and with interactions); adding WLS in one complete model, but without random effects

--- post-analysis/code/proc_auxilary.R
+++ post-analysis/code/proc_auxilary.R
@@ -1,12 +1,14 @@
 # Function to run regression model (#mmix, with spec formula)
-regmodel <- function(formula=~1+I(country_class=='linc') + as.factor(category) + as.factor(brand), 
+regmodel <- function(formula=list(~1+I(country_class=='linc') + as.factor(category) + as.factor(brand)), 
                      dat) {
-
+  
+  if (class(formula)=='formula') formula=list(formula)
+  
   regs <- lapply(unique(dat$variable), function(varname) {
-      st<-lm(update(formula, elast ~ .), data = data.table(dat[variable==varname&!is.na(elast)]), weights=1/elast_se)
-      lt<-lm(update(formula, elastlt ~ .), data = data.table(dat[variable==varname&!is.na(elastlt)]), weights=1/elastlt_se)
-      
-      return(list(variable=varname, st=st, lt=lt))
+    st = lapply(formula, function(form) lm(update(form, elast ~ .), data = data.table(dat[variable==varname&!is.na(elast)]), weights=1/elast_se))
+    lt = lapply(formula, function(form) lm(update(form, elastlt ~ .), data = data.table(dat[variable==varname&!is.na(elastlt)]), weights=1/elastlt_se))
+
+    return(list(variable=varname, st=st, lt=lt))
       
   })
   
@@ -15,20 +17,25 @@ return(regs)
 }
 
 # Function to plot model results (next to each other)
-printout = function(x, type='st', omit='category|brand', title='',printtype='html') {
-  if (type=='st') res = lapply(out, function(m) m$st)
-  if (type=='lt') res = lapply(out, function(m) m$lt)
+printout = function(x, type='st', vars=NULL, omit='category|brand', title='',printtype='html') {
+  if (is.null(vars)) vars=seq(along=x)
   
-  stargazer(res, type = printtype, omit=omit, title = title, column.labels=names(out),
-            notes=c(paste0('ommited fixed effects for: ', gsub('[|]', ', ', omit))))
+  if (type=='st') res = do.call('c', lapply(x[vars], function(m) m$st))
+  if (type=='lt') res = do.call('c', lapply(x[vars], function(m) m$lt))
+  
+  keep_alpha <- function(x) gsub("[^[:alnum:][:space:]]","",x)
+  
+  stargazer(res, type = printtype, omit=omit, title = title, column.labels=names(res), dep.var.caption=NULL, initial.zero=FALSE,
+            notes.align='l',
+            notes=c(paste0('model includes fixed effects for: ', keep_alpha(gsub('[|]', ', ', omit)))))
 
 }
 
 # Function to produce model results for short- and long-term elasticities
-printres <- function(x, omit) {
+printres <- function(x, omit, title='', vars= NULL) {
   
-  printout(x, 'st', omit=omit, title = tab('short-term'))
-  printout(x, 'lt', omit=omit, title = tab('long-term'))
+  printout(x, 'st', omit=omit, title = tab(ifelse(title=='', 'short-term', paste0(title, ', short-term'))), vars=vars)
+  printout(x, 'lt', omit=omit, title = tab(ifelse(title=='', 'long-term', paste0(title, ', long-term'))), vars=vars)
 
 }
 
