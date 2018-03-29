ce34372c16934a99f41394e6f40eb4be782258fb
Hannes Datta <hannes@datta-online.de>
2018-03-29 15:39:40 +0200

    load entire GCI data

--- post-analysis/code/gci.R
+++ post-analysis/code/gci.R
@@ -2,13 +2,19 @@
 library(data.table)
 library(stringr)
 
-gci <- fread('../../../../Data/gci/infrastructure.tsv',header=T)
-gci[, rank:=1:.N]
-gci=melt(gci, id.vars='rank')
+gci <- fread('../../../../Data/gci/gci_data.tsv',header=T)
+gci[, row:=1:.N]
+gci=melt(gci, id.vars='row')
 
-gci[, actual_rank:=as.numeric(unlist(lapply(strsplit(value, ' ',fixed=T), function(x) x[1])))]
-gci[, score:=str_extract(substr(value,4,100), "\\d+\\.*\\d*")]
-gci[, country:=str_trim(gsub('[0-9]|[.]', '', value))]
+gci[, rank:=as.numeric(unlist(lapply(strsplit(value, ' ',fixed=T), function(x) x[1])))]
+gci[, score:=str_extract(substr(gsub('[,]', '', value),4,100), "\\d+\\.*\\d*")]
+
+# clean countries
+gci[, country:=sapply(value, function(x) strsplit(x, '[.]')[[1]][1])]
+gci[, country:=gsub('n/a', '' , country,fixed=T)]
+gci[, country:=str_trim(gsub('[0-9]', '', country))]
+gci <- gci[!grepl('RANK COUNTRY', country)]
+
 
 # keep focal countries
 countries <- c('Australia', 'Singapore', 'Japan', 'New Zealand', 'Hong Kong SAR', 'Korea, Rep',
@@ -24,16 +30,7 @@ length(table(gci[country%in%countries]$country))
 gci <- gci[country%in%countries]
 
 # label contructs
-gci[, construct:='']
-gci[variable=='2.01', construct:='overall_infra']
-gci[variable=='2.02', construct:='roadqual']
-gci[variable=='2.03', construct:='railroadqual']
-gci[variable=='2.04', construct:='portqual']
-gci[variable=='2.05', construct:='airqual']
-gci[variable=='2.06', construct:='airseatkm']
-gci[variable=='2.07', construct:='electricityqual']
-gci[variable=='2.08', construct:='fixedphone']
-gci[variable=='2.09', construct:='mobilephone']
+gci[, construct:=paste0('gci', variable)]
 
 # relabel countries
 gci[, country :=tolower(country)]
@@ -42,7 +39,7 @@ gci[country=='korea, rep', country :='south korea']
 gci[country=='taiwan, china', country :='taiwan']
 
 # drop unnecessary variables
-gci[, ':=' (rank=NULL, value=NULL)]
+gci[, ':=' (row=NULL, value=NULL, variable=NULL)]
 
 gci_save=dcast(gci, country~construct, value.var='score')
 
