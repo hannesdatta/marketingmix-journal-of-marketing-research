d1c9567ac17fb3e2bad66e6adf4b891bfc856451
Hannes Datta <hannes@datta-online.de>
2018-09-27 15:25:49 -0400

    generate temp file with brands in each market, used as input to (potentially) obtain ad data

---
+++ analysis/code/brand_overview.R
@@ -0,0 +1,41 @@
+#     _____    __   _  __                               _                 _       
+#    / ____|  / _| | |/ /       /\                     | |               (_)      
+#   | |  __  | |_  | ' /       /  \     _ __     __ _  | |  _   _   ___   _   ___ 
+#   | | |_ | |  _| |  <       / /\ \   | '_ \   / _` | | | | | | | / __| | | / __|
+#   | |__| | | |   | . \     / ____ \  | | | | | (_| | | | | |_| | \__ \ | | \__ \
+#    \_____| |_|   |_|\_\   /_/    \_\ |_| |_|  \__,_| |_|  \__, | |___/ |_| |___/
+#                                                            __/ |                
+#                                                           |___/                 
+#
+#  _________________________
+# |  _______________  |     |
+# | |               | |1 2 3|
+# | |               | |4 5 6|
+# | |               | |7 8 9|
+# | |               | |# # #|
+# | |               | | ___ |
+# | |_______________| ||___|| 
+# |___________________|_____|
+
+### LOAD DATA SETS
+library(data.table)
+
+## Load panel data
+	brand_panel=fread('../temp/preclean.csv')
+	brand_panel[, ':=' (date = as.Date(date))]
+
+## 
+  brand_panel[, ':=' (first_date=min(date[!is.na(usales)]), last_date=max(date[!is.na(usales)])),by=c('category','country')]
+  
+  tmp=	brand_panel[, list(first_date_cat = unique(first_date), last_date_cat=unique(last_date), first_date_brand=min(date[!is.na(usales)]), last_date_brand=max(date[!is.na(usales)])),by=c('category','country','brand')]
+
+	
+  fwrite(tmp, '../temp/brands_for_advertising.csv',row.names=F) 
+  
+
+## Obs per market
+	tmp=brand_panel[, list(obs=length(unique(date[!is.na(nov6)]))),by=c('market_id','category','country')]
+	setorder(tmp, obs)
+
+	
+	
