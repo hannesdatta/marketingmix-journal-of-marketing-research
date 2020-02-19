371d773ee74713b2623d82af7da0984238a229bb
Hannes Datta <h.datta@tilburguniversity.edu>
2020-02-20 00:22:47 +0100

    after meeting w/ harald, marnik

---
+++ analysis/code/meeting_fiddle.R
@@ -0,0 +1,48 @@
+
+mid = 1
+bid = 1
+
+
+source('proc_analysis_agg.R')
+source('proc_analysis.R')
+source('proc_analysis_brand.R')
+source('proc_ardl.R')
+
+
+
+out=sapply(unique(brand_panel$brand_id)[3], function(bid) try(analyze_brand(bid, quarters=T), silent=T), simplify=F)
+
+
+
+
+# "retrieve" resulting model, reestimate
+
+# simulation
+dv='lnusales'
+dt=data.table(brand_panel[brand_id==3])
+vars = c('lnrwpspr','lnllen','lnwpswdst')
+m<-ardl(type='ardl-ec', dt = dt, dv = dv, vars = c(vars, quarter_vars), exclude_cointegration = NULL,
+        adf_tests= NULL, maxlag = 6, pval = .1)
+
+#shockvariable = vars[1]
+shockvalue = log(1.1)
+
+msim = dynardl(m$tested_model_specs$formula, data = dt, lags = m$tested_model_specs$lagstructure[[m$mchoice]]$lags,
+               diffs = m$tested_model_specs$diffs, lagdiffs = m$tested_model_specs$lagstructure[[m$mchoice]]$lagdiff,
+               levels= m$tested_model_specs$levels, ec = m$tested_model_specs$ec, trend = m$tested_model_specs$trend,
+               simulate = T, shockvar='lnrwpspr', range=48, time = 10, shockval=shockvalue, burnin=12, sig=90, fullsims = T)
+
+dynardl.simulation.plot(msim, type='area', response='levels') #levels') #diffs
+
+dynardl.simulation.plot(msim, type='area', response='levels') #levels') #diffs
+
+dynardl.simulation.plot(msim, type='area', response='shock.effect.decay') #levels') #diffs
+
+# this is the right one!
+
+dynardl.simulation.plot(msim, type='area', response='cumulative.diffs') #levels') #diffs
+
+
+#ardl-firstdiff
+#ardl-levels
+
