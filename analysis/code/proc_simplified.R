e2a38ffe0a087bf1f58cff0f7a4c55f991a1ce52
Hannes Datta <h.datta@tilburguniversity.edu>
2020-12-04 10:53:37 +0100

    add copula correction to EC model

--- analysis/code/proc_simplified.R
+++ analysis/code/proc_simplified.R
@@ -88,7 +88,8 @@ return(list(elast=elast1, model=m1, vif=vif1))
 # Configure model type and calibrate lag structure
 simple_ec <- function(id, controls_diffs='^comp[_]', #
                           controls_laglevels = '',
-                          controls_curr = 'quarter[1-3]|lnholiday') {
+                          controls_curr = 'quarter[1-3]|lnholiday',
+                          controls_cop = '^cop[_]d[.]1[.]', pval = .1) {
   
   # 1.0 Conduct bounds test
   dv='lnusales'
@@ -110,30 +111,47 @@ simple_ec <- function(id, controls_diffs='^comp[_]', #
   vars_delta = paste0('d', c(vars, control_vars$diffs))
   vars_lags = paste0('lag', c(vars, control_vars$lags))
   vars_curr = control_vars$curr
+
+  if (!is.null(controls_cop)) {
+    vars_cop = grep(controls_cop, colnames(dt), value=T)
+    vars_cop = vars_cop[unlist(lapply(dt[, vars_cop, with=F], use_ts))]
+  } else {
+    vars_cop = NULL
+  }
   
   for (v in vars_lags) dt[, (v):=c(NA, get(gsub('^lag','',v))[-.N])]
   
-  my_form = update.formula(dlnusales~1, as.formula(paste0('.~.+1+', paste0(c(vars_delta, 'lnlagusales', paste0('I(-', vars_lags,')'), vars_curr), collapse='+'))))
+  my_form = update.formula(dlnusales~1, as.formula(paste0('.~.+1+', paste0(c(vars_delta, vars_cop, 'lnlagusales', paste0('I(-', vars_lags,')'), vars_curr), collapse='+'))))
   
-  m2 = lm(my_form, data= dt)
+  m = lm(my_form, data= dt)
   
   identifiers = unique(dt[,c('market_id', 'category','country', 'brand', 'brand_id' ),with=F], by=c('brand_id'))
   setkey(identifiers, market_id, brand)
   
-  if (any(is.na(m2$coefficients))) {
-      kickoutcoef = names(m2$coefficients[is.na(m2$coefficients)])
-      my_form =update.formula(my_form, as.formula(paste0('.~.-', paste0(kickoutcoef, collapse='-'))))
-      m2 = lm(my_form, data= dt)
-      
-    }
-
+  # kickout coefs with NA values (e.g., attributes that are not identified)
+  kickoutcoef = NULL
+  if (any(is.na(m$coefficients))) kickoutcoef = c(kickoutcoef, names(m$coefficients[is.na(m$coefficients)]))
+  
+  # kickout ns. copulas
+  tmpres = data.table(variable=names(m$coefficients), summary(m)$coefficients)[grepl(controls_cop, variable)]
+  setnames(tmpres, c('variable','est','se','t','p'))
+  tmpres = tmpres[p>pval]
+  
+  if (nrow(tmpres)>0) kickoutcoef = c(kickoutcoef, tmpres$variable)
+  
+  if (length(kickoutcoef>0)) {
+    my_form =update.formula(my_form, as.formula(paste0('.~.-', paste0(kickoutcoef, collapse='-'))))
+    m2 = lm(my_form, data= dt)
+  } else {m2=m}
+  
   elast2 = rbindlist(lapply(vars, function(v) {
     st=deltaMethod(m2, paste0('(d', v, ')'))
     lt=deltaMethod(m2, paste0('(`I(-lag', v, ')`)/(lnlagusales)'))
     
     data.frame(variable=v, elast = st$Estimate, elast_se=st$SE,
                elastlt = lt$Estimate, elastlt_se = lt$SE, beta = m2$coefficients[paste0('d',v)], 
-               carryover = m2$coefficients['lnlagusales'])
+               carryover = m2$coefficients['lnlagusales'],
+               orig_results = m$coefficients, kicked_out_coefs = kickoutcoef)
     
   }))
   
