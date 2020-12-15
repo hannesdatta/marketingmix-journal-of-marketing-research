2c2207af7cbcdc98f1b7eee8dae001c01c79f8f4
Hannes Datta <h.datta@tilburguniversity.edu>
2020-12-15 09:32:37 +0100

    generate yearly category specific averages

--- post-analysis/code/panel_covariates.R
+++ post-analysis/code/panel_covariates.R
@@ -24,14 +24,43 @@ setkey(brand_panel, category, country, brand)
 for (.col in cols) eval(parse(text=paste0('brand_panel[mix, ', .col, '_index:=i.', .col, '_index]')))
 for (.col in cols) eval(parse(text=paste0('brand_panel[mix, ', .col, '_std:=i.', .col, '_std]')))
 
+# define metrics to vary only at the yearly level for brands
+vars <- grep('2010$|avg$', grep('wgi_|gdp|gini', colnames(brand_panel),value=T), value=T, invert=T)
+
+tmp = unique(brand_panel, by = c('category','country','year'))[, lapply(.SD, mean,na.rm=T), by = c('category','country'), .SDcol=vars]
+tmp <- tmp[,unlist(lapply(tmp,function(x) all(is.na(x))))==F,with=F]
+for (.v in vars) if (.v %in% colnames(tmp)) setnames(tmp, .v, paste0(.v,'yravg'))
+
+year_averages <- copy(tmp)
+
+# log continues variables which are non-negative
+   for (.var in setdiff(colnames(year_averages), c('category','country'))) {
+      # dummy var
+      vrs <- unlist(year_averages[, .var,with=F])
+      #print(.var)
+      if (class(vrs)=='character') next
+      if (all(vrs %in% c(NA,1,0))) next
+      if (all(is.na(vrs) | vrs >= 0)) year_averages[, paste0('ln_', .var):=log(get(.var)+1)]
+      if (all(is.na(vrs) | vrs > 0)) year_averages[, paste0('ln_', .var):=log(get(.var))]
+      
+   }
+
+fwrite(year_averages, paste0('../output/covariates-yearaverages_category-country.csv'))
+
 # aggregation
 agg_units = list(c('country'), c('country', 'category'),
+                 c('country', 'category', 'year'),
                  c('country','category','brand'))
 
 out=rbindlist(lapply(agg_units, function(agg) {
   #agg = agg_units[[1]]
   
-  tmp = brand_panel[, lapply(.SD, uniqueN), by = agg]
+  #tmp = brand_panel[, lapply(.SD, uniqueN), by = agg]
+  
+  .vars <- setdiff(colnames(brand_panel), agg)
+  tmpx=rbindlist(lapply(.vars, function(.v) brand_panel[, list(.N),by=c(agg, .v)][, list(variable=.v, N=.N),by=c(agg)]))
+  tmp=dcast(tmpx, as.formula(paste0(paste0(agg, collapse='+'),'~variable')), value.var='N')
+  
   tmp2=unlist(lapply(tmp, function(x) all(x==1)))
   res=data.table(variable = names(tmp2)[which(tmp2==T)])
   res[, agg_factor:=paste(agg, collapse='-')]
@@ -69,6 +98,7 @@ dt <- lapply(tmp, function(x) {
    if (all(is.na(vrs) | vrs > 0)) tmp[, paste0('ln_', .var):=log(get(.var))]
    
  }
+ 
  cat(paste0('Covariates for: ', unique(x$agg_factor), '\n=========================================\n'))
  print(data.frame(variable=colnames(tmp)))
  
