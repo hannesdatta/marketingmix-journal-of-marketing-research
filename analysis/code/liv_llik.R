2eb4a081c1b09e01609a2ab015a7cb56c908c906
Hannes Datta <h.datta@tilburguniversity.edu>
2021-03-15 14:04:14 +0100

    implement multiple endogenous instruments; verification with branch revision is pending

--- analysis/code/liv_llik.R
+++ analysis/code/liv_llik.R
@@ -1,30 +1,56 @@
 
 library(mvtnorm)
 
-map_pars <- function(pars, levels = 2) {
-  beta0 = pars[1]
-  beta1 = pars[2]
+map_pars <- function(pars, levels = 2, endogenous_variables = 1) {
+  
+  # Parameters
+  
+  # Means of latent instruments --> levels x endogeneous variables
+  # Class probabilities of latent instruments --> (levels-1) x endogenous variables
+  # Beta coefficients of effect of LIV on outcome variable
+  
+  # Covariance matrix of focal equation and endogenous regressors: 1 + endogenous variables (but then only lower diag)
+  istart = 1
+  
+  .mapping = seq(from=istart, length.out=levels * endogenous_variables)
+  .lambdas = matrix(pars[.mapping],ncol=endogenous_variables)
+  lambdas = apply(.lambdas, 2, function(x) cumsum(c(x[1], exp(x[-1]))))
+  
+  istart = max(.mapping) + 1
+  
+  
+  .mapping = seq(from=istart,length.out=(levels-1)*endogenous_variables)
+  .prob = matrix(pars[.mapping],ncol=endogenous_variables)
+  
+  prob = apply(rbind(0, .prob), 2, function(x) exp(x)/sum(exp(x)))
+  
+  istart = max(.mapping) + 1
   
   # Covariance/correlation structure
-  uchol = diag(2)
-  uchol[1,1] <- pars[3]
-  uchol[1,2] <- pars[4]
-  uchol[2,2] <- pars[5]
+  .uchol = diag(endogenous_variables + 1)
+  cov_nparameters = .5 * (endogenous_variables+1)*(endogenous_variables+2)
+  .mapping = seq(from=istart, length.out = cov_nparameters)
+  .uchol[upper.tri(.uchol,diag=T)] <- pars[.mapping]
+  sigma <- crossprod(.uchol)
   
-  sigma <- crossprod(uchol)
   
-  # class memberships
-  istart=6
+  # coefficients of endogenous variables
+  istart = max(.mapping) + 1
   
-  .prob = exp(c(0, pars[seq(from=istart,length.out=levels-1)]))
-  prob = .prob/sum(.prob)
+  .mapping = seq(from=istart, length.out=endogenous_variables)
+  gamma = pars[.mapping]
   
-  istart = istart+levels-1
+  #remaining parameters
+  istart = max(.mapping) + 1
+  .mapping = seq(from=istart, length.out=length(pars)-istart+1)
+  betas = pars[.mapping]
   
-  .lambdas = pars[seq(from=istart, length.out=levels)] 
-  lambdas = cumsum(c(.lambdas[1], exp(.lambdas[-1])))
-  
-  return(list(beta0=beta0, beta1=beta1, uchol=uchol, sigma=sigma, prob=prob, lambdas=lambdas))
+  return(list(betas=betas, gamma=gamma, 
+              sigma = sigma,
+              prob = prob,
+              lambdas = lambdas,
+              uchol=.uchol, prob_untransformed=.prob,
+              lambdas_untransformed = .lambdas))
 }
 
 reps=100
@@ -35,31 +61,42 @@ if (class(N)=='try-error') N=120
 simvals = matrix(runif(N*reps),ncol=100)
 
 
-llik <- function (params, levels = 2) {
+llik <- function(params, levels = 2, endogenous_variables = 2, data = list(y=y,X=cbind(rep(1,length(y))),
+                                                                            endog=cbind(x,x))) {
   
-  pars=map_pars(params, levels = levels)
+  pars=map_pars(params, levels = levels, endogenous_variables = endogenous_variables)
   
   lambdas = pars$lambdas
-  beta0=pars$beta0
-  beta1=pars$beta1
+  betas=pars$betas[1]
+  
+  gamma=pars$gamma
   
   prob=pars$prob
   varcov=pars$sigma
 
-  y_pred = sapply(lambdas, function(lambda) beta0 + beta1 * lambda)
+  iterating_lambdas = as.matrix(expand.grid(split(drop(lambdas), rep(1:endogenous_variables,each=levels))))
   
-  liks = sapply(seq(from=1, to=levels), function(l) {
-    dmvnorm(cbind(y-y_pred[l], x-lambdas[l]), mean=c(0,0), sigma=varcov, log=T)
+  iterating_probabilities = apply(expand.grid(split(drop(prob), rep(1:endogenous_variables,each=levels))),1,prod)
+  
+  
+  
+  liks = sapply(seq(from=1, to=length(iterating_probabilities)), function(l) {
+    .lambda= iterating_lambdas[l,]
+    y_pred = data$X%*%betas + drop(t(cbind(.lambda))%*%cbind(gamma))
+    
+    .tmp = cbind(y-y_pred, data$endog - matrix(rep(.lambda,nrow(data$endog)),ncol=endogenous_variables, byrow=T))
+    
+    dmvnorm(.tmp, mean=rep(0, each=endogenous_variables+1), sigma=varcov, log=T)
   })
   
   
-  lprob = matrix(rep(log(prob),each=length(y)),ncol=levels)
+  lprob = matrix(rep(log(iterating_probabilities),each=length(y)),ncol=length(iterating_probabilities))
   
   max.AB = apply(lprob+liks,1,max)
   
   llik_min_maxab = apply(liks, 2, function(x) exp(x- max.AB))
   
-  prob_times_exp = sapply(seq(from=1, to=levels), function(i) prob[i]*llik_min_maxab[,i])
+  prob_times_exp = sapply(seq(from=1, to=length(iterating_probabilities)), function(i) iterating_probabilities[i]*llik_min_maxab[,i])
   
   #llik_lse = sum(max.AB + log(prob[1] * exp(llik1 - max.AB) + prob[2] * exp(llik2-max.AB)))
   #llik_lse = sum(max.AB + log(prob[1] * llik_min_maxab[,1] + prob[2] * llik_min_maxab[,2]))
