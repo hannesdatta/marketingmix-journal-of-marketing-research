69b4172216cec2f002a27a25da30f288af3c0cde
Hannes Datta <h.datta@tilburguniversity.edu>
2021-05-07 10:44:11 +0200

    improve data/reduce file size of RData app

--- post-analysis/code/get_externals.bat
+++ post-analysis/code/get_externals.bat
@@ -2,6 +2,8 @@ CALL RMDIR ..\externals /S /Q
 CALL RMDIR ..\temp /S /Q
 CALL mkdir ..\externals
 CALL mkdir ..\temp
-copy ..\..\analysis\output\*.* ..\externals\
 copy ..\..\analysis\temp\preclean*.* ..\externals\
+copy ..\..\analysis\output\*.csv ..\externals\
+copy ..\..\analysis\output\*.txt ..\externals\
+copy ..\..\analysis\output\*.html ..\externals\
 pause
